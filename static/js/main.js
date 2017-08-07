0d3d450c993372264bf92478d2593e9fbed8da7a
Paul Giron <paul@phntms.com>
2017-08-07 15:01:31 +0100

    Space design

--- static/js/main.js
+++ static/js/main.js
@@ -5,20 +5,10 @@ Photol.Main = function() {
 
 // Init function
 Photol.Main.prototype.init_ = function() {
-  console.log('Init main constructor');
+  // console.log('Init main constructor');
 
-  var $scene = $('.parallax-wrapper').parallax({
-    // calibrateX: true,
-    // calibrateY: true,
-    // invertX: false,
-    // invertY: true,
-    limitX: 20,
-    limitY: 10,
-    // scalarX: 2,
-    // scalarY: 8,
-    // frictionX: 0.2,
-    // frictionY: 0.8,
-    // originX: 0.0,
-    // originY: 1.0
+  var $scene = $('#sun').parallax({
+    // limitX: 20,
+    // limitY: 10,
   });
 };
