d482cf07cd1169a7cef30e8d6c106950fbd8aaf9
Paul Giron <pol.giron@gmail.com>
2017-10-25 18:38:28 +1300

    Parallax

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,21 @@
 PG.App = function() {
   // new PG.Main();
   new PG.Particles();
+
+  $('#sun').parallax({
+    // calibrateX: true,
+    // calibrateY: true,
+    // invertX: false,
+    // invertY: true,
+    // limitX: 20,
+    // limitY: 10,
+    // scalarX: 2,
+    // scalarY: 8,
+    // frictionX: 0.2,
+    // frictionY: 0.8,
+    // originX: 0.0,
+    // originY: 1.0
+  });
 };
 
 var app = new PG.App();
