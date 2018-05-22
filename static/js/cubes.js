d9c2635375e1880e010e0863cda97df956e90114
Paul Giron <pol.giron@gmail.com>
2018-05-22 13:10:10 +1200

    Disable mouse animation, responsive design

--- static/js/cubes.js
+++ static/js/cubes.js
@@ -42,6 +42,108 @@ PG.Cubes = function() {
   this.delay_ = 30;
   this.speed_ = 700
 
+  // Links to hover
+  this.links_ = document.querySelectorAll('.email, .phone');
+
+  this.addCubes_();
+  this.addPivot_();
+  this.addListeners_();
+  this.start_();
+};
+
+PG.Cubes.prototype.addListeners_ = function() {
+  window.addEventListener('resize', this.onWindowResize_.bind(this), false);
+  // document.addEventListener('mousemove', this.onMouseMove_.bind(this), false);
+
+  this.links_.forEach(function(element) {
+    element.addEventListener('mouseenter', this.onMouseHover_.bind(this), false);
+  }.bind(this));
+};
+
+PG.Cubes.prototype.onWindowResize_ = function() {
+  this.camera_.aspect = window.innerWidth / window.innerHeight;
+  this.camera_.updateProjectionMatrix();
+  this.renderer_.setSize(window.innerWidth, window.innerHeight);
+};
+
+PG.Cubes.prototype.onMouseHover_ = function(event) {
+  // console.log(event.target);
+};
+
+PG.Cubes.prototype.onMouseMove_ = function(event) {
+  var x = event.clientX;
+  // var y = event.clientY;
+  var length = window.innerWidth / 2;
+  var toY = 4;
+  // var delta = 3;
+
+  if (x < window.innerWidth / 2) {
+    for (var i = 0; i < this.cubes_.length; i++) {
+      // var toY = -(this.cubes_[i].defaultY - delta);
+      var cubeY = -(toY - ((toY + this.cubes_[i].defaultY) * x / length));
+      this.cubes_[i].position.y = cubeY;
+    }
+  }
+};
+
+PG.Cubes.prototype.addHelpers_ = function() {
+  var axes = new THREE.AxesHelper(20);
+  var helper = new THREE.GridHelper(100, 10);
+  this.scene_.add(axes);
+  this.scene_.add(helper);
+
+  var lightHelper = new THREE.SpotLightHelper(this.spotLight_);
+  this.scene_.add(lightHelper);
+};
+
+PG.Cubes.prototype.addCube_ = function(args) {
+  var material = new THREE.MeshPhongMaterial({color: args.color, dithering: true});
+  var geometry = new THREE.BoxBufferGeometry(args.size1, 20, args.size3);
+  var cube = new THREE.Mesh(geometry, material);
+  // cube.position.set(args.x, args.y, args.z);
+  cube.position.set(args.x, -22, args.z);
+  this.cubeWrapper_.add(cube);
+  cube.defaultY = args.y;
+  cube.delay = args.delay;
+  this.cubes_.push(cube);
+};
+
+PG.Cubes.prototype.addPivot_ = function() {
+  this.pivot_ = new THREE.Group();
+  this.scene_.add(this.pivot_);
+  this.pivot_.add(this.cubeWrapper_);
+  this.cubeWrapper_.position.set(-7, -6, 2);
+  this.pivot_.rotation.x = .2;
+  this.pivot_.rotation.y = .6;
+  this.pivot_.rotation.z = -.65;
+};
+
+PG.Cubes.prototype.animateCube_ = function(element, toY, speed, delay) {
+  var tween = new TWEEN.Tween(element.position).to({y: toY}, speed);
+  tween.delay(delay);
+  tween.easing(TWEEN.Easing.Quintic.Out);
+  // tween.repeat(Infinity);
+  tween.yoyo(true);
+  tween.start();
+};
+
+PG.Cubes.prototype.start_ = function() {
+  // Start loop
+  this.loop_();
+
+  // Animate cubes
+  for (var i = 0; i < this.cubes_.length; i++) {
+    this.animateCube_(this.cubes_[i], this.cubes_[i].defaultY, this.speed_, this.cubes_[i].delay);
+  }
+};
+
+PG.Cubes.prototype.loop_ = function() {
+  window.requestAnimationFrame(this.loop_.bind(this));
+  TWEEN.update();
+  this.renderer_.render(this.scene_, this.camera_);
+};
+
+PG.Cubes.prototype.addCubes_ = function() {
   this.addCube_({
     x: 0,
     y: 0,
@@ -195,88 +297,4 @@ PG.Cubes = function() {
     color: this.colors_.orange,
     delay: this.delay_ * 7
   });
-
-  this.addPivot_();
-
-  this.start_();
-
-  window.addEventListener('resize', this.onWindowResize_.bind(this), false);
-  document.addEventListener('mousemove', this.onMouseMove_.bind(this), false);
-};
-
-PG.Cubes.prototype.onWindowResize_ = function() {
-  this.camera_.aspect = window.innerWidth / window.innerHeight;
-  this.camera_.updateProjectionMatrix();
-  this.renderer_.setSize(window.innerWidth, window.innerHeight);
-};
-
-PG.Cubes.prototype.onMouseMove_ = function(event) {
-  var x = event.clientX;
-  // var y = event.clientY;
-  var length = window.innerWidth / 2;
-  var toY = 4;
-  // var delta = 3;
-
-  if (x < window.innerWidth / 2) {
-    for (var i = 0; i < this.cubes_.length; i++) {
-      // var toY = -(this.cubes_[i].defaultY - delta);
-      var cubeY = -(toY - ((toY + this.cubes_[i].defaultY) * x / length));
-      this.cubes_[i].position.y = cubeY;
-    }
-  }
-};
-
-PG.Cubes.prototype.addHelpers_ = function() {
-  var axes = new THREE.AxesHelper(20);
-  var helper = new THREE.GridHelper(100, 10);
-  this.scene_.add(axes);
-  this.scene_.add(helper);
-
-  var lightHelper = new THREE.SpotLightHelper(this.spotLight_);
-  this.scene_.add(lightHelper);
-};
-
-PG.Cubes.prototype.addCube_ = function(args) {
-  var material = new THREE.MeshPhongMaterial({color: args.color, dithering: true});
-  var geometry = new THREE.BoxBufferGeometry(args.size1, 20, args.size3);
-  var cube = new THREE.Mesh(geometry, material);
-  // cube.position.set(args.x, args.y, args.z);
-  cube.position.set(args.x, -22, args.z);
-  this.cubeWrapper_.add(cube);
-  cube.defaultY = args.y;
-  cube.delay = args.delay;
-  this.cubes_.push(cube);
-};
-
-PG.Cubes.prototype.addPivot_ = function() {
-  this.pivot_ = new THREE.Group();
-  this.scene_.add(this.pivot_);
-  this.pivot_.add(this.cubeWrapper_);
-  this.cubeWrapper_.position.set(-7, -6, 2);
-  this.pivot_.rotation.x = .2;
-  this.pivot_.rotation.y = .6;
-  this.pivot_.rotation.z = -.65;
-};
-
-PG.Cubes.prototype.animateCube_ = function(element, toY, speed, delay) {
-  var tween = new TWEEN.Tween(element.position).to({y: toY}, speed);
-  tween.delay(delay);
-  tween.easing(TWEEN.Easing.Quintic.Out);
-  tween.start();
-};
-
-PG.Cubes.prototype.start_ = function() {
-  // Start loop
-  this.loop_();
-
-  // Animate cubes
-  for (var i = 0; i < this.cubes_.length; i++) {
-    this.animateCube_(this.cubes_[i], this.cubes_[i].defaultY, this.speed_, this.cubes_[i].delay);
-  }
-};
-
-PG.Cubes.prototype.loop_ = function() {
-  window.requestAnimationFrame(this.loop_.bind(this));
-  TWEEN.update();
-  this.renderer_.render(this.scene_, this.camera_);
 };
