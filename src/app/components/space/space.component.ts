b1878dda099ee6d77a3fd68aeed63f77c7bd98b3
Paul Giron <pol.giron@gmail.com>
2020-11-23 01:07:16 +0100

    Update

--- src/app/components/space/space.component.ts
+++ src/app/components/space/space.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { InfosService } from 'src/app/services/infos.service';
+import { getRandomInt } from 'src/app/utils/utils';
 
 @Component({
   selector: 'app-space',
@@ -14,8 +15,22 @@ export class SpaceComponent implements OnInit {
     private infos: InfosService
   ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.email = this.infos.email;
     this.phone = this.infos.phone;
+    console.log(getRandomInt(20, 100) / 100);
+  }
+
+  numberOfStars(n: number): any[] {
+    return Array(n);
+  }
+
+  get randomProperties(): {left: string, top: string, width: string, delay: string} {
+    return {
+      left: getRandomInt(0, window.innerWidth / 1.5) + 'px',
+      top: getRandomInt(0, window.innerHeight - 80) + 'px',
+      width: getRandomInt(10, 14) + 'px',
+      delay: getRandomInt(1000, 5000) + 'ms'
+    }
   }
 }
